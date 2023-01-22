95dfcc27d981cafde9a5ce7dc4d14551dbe6f598
Aarish <aarishm767@gmail.com>
2023-01-23 02:11:17 +0530

    Initial Commit

---
+++ src/main/java/com/example/userlaptop/UserLaptopApplication.java
@@ -0,0 +1,13 @@
+package com.example.userlaptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLaptopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLaptopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userlaptop/UserLaptopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userlaptop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLaptopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
